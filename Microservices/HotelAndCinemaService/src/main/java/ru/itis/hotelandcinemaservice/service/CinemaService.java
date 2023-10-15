2d54b5c9d9fb33e49c006ac2d71e5bc9745df5ed
Alexsey Chugunov <43906446+beatztw@users.noreply.github.com>
2023-10-15 17:33:15 +0300

    replace service

--- Microservices/HotelAndCinemaService/src/main/java/ru/itis/hotelandcinemaservice/service/CinemaService.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.hotelandcinemaservice.service;
-
-import ru.itis.hotelandcinemaservice.dto.CinemasPage;
-
-public interface CinemaService {
-    CinemasPage getAllCinemas(Integer page);
-
-    CinemasPage getAllCinemasByCity(String city, Integer page);
-}
--- Microservices/HotelAndCinemaService/src/main/java/ru/itis/hotelandcinemaservice/service/HotelService.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.hotelandcinemaservice.service;
-
-import ru.itis.hotelandcinemaservice.dto.HotelsPage;
-
-public interface HotelService {
-    HotelsPage getAllHotels(Integer page);
-
-    HotelsPage getAllHotelsByCity(String city, Integer page);
-}
