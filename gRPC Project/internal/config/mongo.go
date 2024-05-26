e35d0b03e81a4b96d255e2f1b8651685aac93fd3
Alexsey Chugunov <43906446+beatztw@users.noreply.github.com>
2024-05-26 13:13:21 +0300

    homework 2

---
+++ gRPC Project/internal/config/mongo.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoConfig struct {
+	URI  string
+	Name string
+}
+
+func SetupMongoDataBase(ctx context.Context) (*mongo.Database, error) {
+	config := &MongoConfig{}
+
+	err := viper.UnmarshalKey("mongo.database", config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(config.URI))
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(ctx, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client.Database(config.Name), nil
+
+}
